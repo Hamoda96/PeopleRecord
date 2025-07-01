b9eb1b1c11ccc590541d8644901fe812718f9d56
hamoda96 <mahmoud.m.hamoda@gmail.com>
2025-07-01 21:27:22 +0300

    add users xml view

--- app/src/main/java/com/hamoda/peoplerecord/presentation/users/UsersFragment.kt
+++ app/src/main/java/com/hamoda/peoplerecord/presentation/users/UsersFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.core.net.toUri
@@ -17,8 +15,9 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavDeepLinkRequest
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
+import com.hamoda.peoplerecord.R
 import com.hamoda.peoplerecord.presentation.designsystem.theme.PeopleRecordTheme
-import com.hamoda.peoplerecord.presentation.user.UserSideEffect
+import com.hamoda.peoplerecord.presentation.users.xml.xmlUsersView
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import kotlin.getValue
@@ -26,20 +25,36 @@ import kotlin.getValue
 class UsersFragment : Fragment() {
 
     private val viewModel: UsersViewModel by inject()
+    private var useCompose = false
+    private lateinit var binding: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Optionally restore useCompose from savedInstanceState
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return ComposeView(requireContext()).apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-
-            setContent {
-                PeopleRecordTheme {
-                    AddUserScreen(onEvent = viewModel::onEvent)
+        if (useCompose) {
+            return ComposeView(requireContext()).apply {
+                setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+                setContent {
+                    PeopleRecordTheme {
+                        UserListScreenContent(
+                            onEvent = viewModel::onEvent,
+                            users = viewModel.users.collectAsStateWithLifecycle(emptyList()).value,
+                            loading = viewModel.loading.collectAsStateWithLifecycle(false).value
+                        )
+                    }
                 }
             }
+        } else {
+            binding = inflater.inflate(R.layout.users_fragment, container, false)
+            xmlUsersView(fragment = this, view = binding, viewModel = viewModel)
+            return binding
         }
     }
 
@@ -63,12 +78,5 @@ class UsersFragment : Fragment() {
                 }
             }
         }
-    }
-
-    @Composable
-    private fun AddUserScreen(onEvent: (UsersEvent) -> Unit) {
-        val users by viewModel.users.collectAsStateWithLifecycle()
-        val loading by viewModel.loading.collectAsStateWithLifecycle()
-        UserListScreenContent(onEvent = onEvent, users = users, loading = loading)
     }
 }
